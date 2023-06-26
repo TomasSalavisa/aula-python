c13a30b38a0b44b866ad108335de9d590f9a9e17
Tomas_Sousa <sousa.tommy@gmail.com>
2023-06-26 15:12:12 +0100

    update

--- 31-05-2023/teste3.py
+++ 31-05-2023/teste3.py
@@ -1,46 +1,46 @@
-def escreverNome():
-	name = input("Whats your name: ")
-	age = input('Whats your age: ')
-	sex = input('Whats your gender: ')
-	with open('names.txt', 'a') as file:
-		file.write(f'------------Profile-------------\nName: {name}\nAge: {age} \nGender: {sex}\n')
+# def escreverNome():
+# 	name = input("Whats your name: ")
+# 	age = input('Whats your age: ')
+# 	sex = input('Whats your gender: ')
+# 	with open('names.txt', 'a') as file:
+# 		file.write(f'------------Profile-------------\nName: {name}\nAge: {age} \nGender: {sex}\n')
 
-def imprimirNome():
-	with open('names.txt', 'r') as file:
-		for line in file:
-			print(line)
+# def imprimirNome():
+# 	with open('names.txt', 'r') as file:
+# 		for line in file:
+# 			print(line)
 
-def eliminarNomes():
-	with open('names.txt', 'w') as file:
-		print("'Registos apagados")
+# def eliminarNomes():
+# 	with open('names.txt', 'w') as file:
+# 		print("'Registos apagados")
 
-def Menu():
-	print('---------------------------------------------------Lista de Nomes----------------------------------------------------------')
-	print('1 - Inserir Novo Nome')
-	print('2 - Imprimir Nomes da lista')
-	print('3 - Eliminar Nomes da lista')
-
-
-Opcao = 1
-while Opcao in range(1,4):
-	Menu()
-	Opcao = int(input('Qual a sua opção: '))
-	if Opcao == 1:
-		escreverNome()
-	elif Opcao == 2:
-		imprimirNome()
-	elif Opcao == 3:
-		eliminarNomes()
-	else:
-		print("Sair")
-	print()
-	print('Enter para continuar...')
+# def Menu():
+# 	print('---------------------------------------------------Lista de Nomes----------------------------------------------------------')
+# 	print('1 - Inserir Novo Nome')
+# 	print('2 - Imprimir Nomes da lista')
+# 	print('3 - Eliminar Nomes da lista')
 
 
-# dic = [{"Nome" : "Joao", "Idade": "21", "Sexo": "Macho"},
-# 	   {"Nome" : "Tomas", "Idade": "45", "Sexo": "Macho"},
-# 	   {"Nome" : "Miguel", "Idade": "81", "Sexo": "Gigachad"},
-# 	   {"Nome" : "Maria", "Idade": "17", "Sexo": "Female"},]
-# for i in dic:
-# 	print(f'\nNome: {i["Nome"]} \nIdade: {i["Idade"]} \nSexo: {i["Sexo"]}')
+# Opcao = 1
+# while Opcao in range(1,4):
+# 	Menu()
+# 	Opcao = int(input('Qual a sua opção: '))
+# 	if Opcao == 1:
+# 		escreverNome()
+# 	elif Opcao == 2:
+# 		imprimirNome()
+# 	elif Opcao == 3:
+# 		eliminarNomes()
+# 	else:
+# 		print("Sair")
+# 	print()
+# 	print('Enter para continuar...')
+
+
+dic = [{"Nome" : "Joao", "Idade": "21", "Sexo": "Macho"},
+	   {"Nome" : "Tomas", "Idade": "45", "Sexo": "Macho"},
+	   {"Nome" : "Miguel", "Idade": "81", "Sexo": "Gigachad"},
+	   {"Nome" : "Maria", "Idade": "17", "Sexo": "Female"},]
+for i in dic:
+	print(f'\nNome: {i["Nome"]} \nIdade: {i["Idade"]} \nSexo: {i["Sexo"]}')
 	
